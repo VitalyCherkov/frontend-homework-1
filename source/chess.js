f6bc2ba9f7329cb2a4cc338b351557f9b2ec78eb
VitalyCherkov <vv-ch@bk.ru>
2019-02-05 10:50:27 +0300

    fixed

--- source/chess.js
+++ source/chess.js
@@ -1,16 +1,23 @@
 'use strict';
 
+/**
+ * Draws chessboard
+ * 
+ * @param {number} size 
+ * @returns {(string|null)} Chessboard string.
+ */
+
 const chess = (size) => {
-	size = Number.parseFloat(size)
+	size = Number.parseFloat(size);
 	if (!Number.isInteger(size) || size < 2) {
 		return null;
 	}
 
 	const parity = size % 2;
 
-	const initialString = '* '.repeat(size / 2) + '*'.repeat(parity) + '\n';
-	const secondString = ' *'.repeat(size / 2) + ' '.repeat(parity) + '\n';
+	const initialString = '* '.repeat(size / 2) + (parity ? '*' : '') + '\n';
+	const secondString = ' *'.repeat(size / 2) + (parity ? ' ' : '') + '\n';
 	
 	return (initialString + secondString).repeat(size / 2)
-		+ initialString.repeat(parity); 
+		+ (parity ? initialString : ''); 
 }
